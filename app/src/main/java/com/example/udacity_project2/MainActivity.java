eedc23278794443878c5bd2168410b2bcc68c00e
Sascha Brägger <sascha.braegger-android@hotmail.com>
2019-09-05 10:26:08 +0200

    Added if else to define only gain 3 points with conversion if try was succesfull
    Added 2 new variables global for conversion if/else to track status of Try happend or not

--- app/src/main/java/com/example/udacity_project2/MainActivity.java
+++ app/src/main/java/com/example/udacity_project2/MainActivity.java
@@ -11,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
 
     int TeamA = 0;
     int TeamB = 0;
+    int conversion_TeamA = 0;
+    int conversion_TeamB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,43 +23,63 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void button_try(View view) {
+        conversion_TeamA = 1;
         TeamA = TeamA + 5;
         displayForTeamA(TeamA);
     }
 
     public void button_conversion(View view) {
-        TeamA = TeamA + 3;
-        displayForTeamA(TeamA);
+        if (conversion_TeamA == 1) {
+            TeamA = TeamA + 3;
+            displayForTeamA(TeamA);
+            conversion_TeamA = 0;
+
+        } else {
+            displayForTeamA(TeamA);
+        }
+
     }
 
 
     public void button_penalty(View view) {
+
+        conversion_TeamA = 0;
         TeamA = TeamA + 3;
         displayForTeamA(TeamA);
     }
 
     public void button_dropkick(View view) {
         TeamA = TeamA + 3;
+        conversion_TeamA = 0;
         displayForTeamA(TeamA);
     }
 
     public void buttonb_try(View view) {
+        conversion_TeamB = 1;
         TeamB = TeamB + 5;
         displayForTeamB(TeamB);
     }
 
 
     public void buttonb_conversion(View view) {
-        TeamB = TeamB + 3;
-        displayForTeamA(TeamA);
+        if (conversion_TeamB == 1) {
+            TeamB = TeamB + 3;
+            displayForTeamB(TeamB);
+            conversion_TeamB = 0;
+
+        } else {
+            displayForTeamB(TeamB);
+        }
     }
 
     public void buttonb_penalty(View view) {
+        conversion_TeamB = 0;
         TeamB = TeamB + 3;
         displayForTeamB(TeamB);
     }
 
     public void buttonb_dropkick(View view) {
+        conversion_TeamB = 0;
         TeamB = TeamB + 3;
         displayForTeamB(TeamB);
     }
@@ -65,6 +87,8 @@ public class MainActivity extends AppCompatActivity {
     public void reset(View view) {
         TeamA = 0;
         TeamB = 0;
+        conversion_TeamA = 0;
+        conversion_TeamB = 0;
         displayForTeamA(TeamA);
         displayForTeamB(TeamB);
     }
